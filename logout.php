6c12e0c58274b841564de1bc6d142e5c054d1c7b
Akash Dixit <anshudixit00011@gmail.com>
2018-09-13 00:21:09 +0530

    login sys

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>log out</title>
+</head>
+       <center>Sorry!! you'r logout!!</center>
+       <?php session_destroy(); ?>
+<body>
+
+</body>
+</html>
